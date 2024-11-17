b5b4dca04dc64087c9b924b641e09c6e9294c5a7
wanli <kanzihuang@qq.com>
2024-11-17 16:08:35 +0800

    feat: nginx wqgateway

---
+++ tls/tls-simple/start-temporal-postgres.sh
@@ -0,0 +1,12 @@
+# #!/bin/bash
+
+# TEMPORAL_TLS_CERTS_DIR is used in docker-compose.yml to point 
+# to the location of generated test certificates within the container
+export TEMPORAL_TLS_CERTS_DIR=/etc/temporal/config/certs
+
+# TEMPORAL_LOCAL_CERT_DIR is used in docker-compose.yml to point
+# to our local directory with generated certificates to be mounted
+# as a volume at TEMPORAL_TLS_CERTS_DIR within the container
+export TEMPORAL_LOCAL_CERT_DIR=./certs
+
+docker-compose -f docker-compose-postgres.yml up -d
